b1269628dc5763beba45f262dd07a8eb1e42debf
Xavier <xavierbenoit888@gmail.com>
2026-06-08 16:17:58 -0400

    ajout assignation

---
+++ src/main/java/ca/usherbrooke/fgen/api/mapper/AssigneeMapper.java
@@ -0,0 +1,16 @@
+package ca.usherbrooke.fgen.api.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface AssigneeMapper {
+
+    void insertAssignee(@Param("tacheId") String tacheId, @Param("cip") String cip);
+
+    void deleteAssignee(@Param("tacheId") String tacheId, @Param("cip") String cip);
+
+    List<String> selectAssignees(@Param("tacheId") String tacheId);
+}
