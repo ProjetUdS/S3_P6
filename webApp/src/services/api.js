c9f0e8dcb56a390eee3415d4d81d4c7d71b3108b
Adam <69704042+Ad0rdi@users.noreply.github.com>
2026-07-13 23:11:24 -0400

    fix: parsing correcte des dates lors de la modif d'une tâche
    SCRUM-111

--- webApp/src/services/api.js
+++ webApp/src/services/api.js
@@ -138,8 +138,8 @@ export async function updateTache(tacheId, tache) {
   if (tache.nomTache != null) params.set('nomTache', tache.nomTache);
   if (tache.status != null) params.set('status', tache.status);
   if (tache.description != null) params.set('description', tache.description);
-  if (tache.dateDebut != null) params.set('dateDebut', tache.dateDebut);
-  if (tache.dateFin != null) params.set('dateFin', tache.dateFin);
+  if (tache.dateDebut) params.set('dateDebut', tache.dateDebut.replace(/-/g, '/'));
+  if (tache.dateFin) params.set('dateFin', tache.dateFin.replace(/-/g, '/'));
   const response = await api.put(`/tache/${tacheId}?${params.toString()}`);
   return response.data;
 }
